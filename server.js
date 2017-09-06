93dfc7a851c614472194b7b796aec7f560c6723d
Zeeshan Hyder <hyder.zeeshan@live.com>
2017-09-06 19:02:49 -0400

    implemented streming

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var http = express();
 http.use(express.static('public'))
 
 http.get('/', function(req,res){
-  res.send('<div><p>Client is available at: <b>/client</b><p>Server is available at: <b>ws://127.0.0.1:9001</b></p></div>');
+  res.send('<div><p>Client is available at: <a href="client.html"><b>/client.html</b></a><p>Server is available at: <b>ws://127.0.0.1:9001</b></p></div>');
 });
 
 
@@ -19,16 +19,12 @@ http.listen(9000, function () {
   console.log('INFO: Webserver fired up on port 9000');
 })
 
-// WebSocket Handler
-const ws = new WebSocket('ws://localhost:9001',{
-  perMessageDeflate: false
-}, function(){
-  console.log('INFO: WebSocket Server fired up on port 9001')
-});
 
 // Fire up WebSocket server
 const wsServer = new WebSocket.Server({
   port: 9001
+}, function(){
+  console.log('INFO: WebSockets server fired up on port 9001\n\n');
 });
 
 
@@ -45,42 +41,95 @@ function incomingMessageHandler(payload){
 }
 
 
-function sendCSVFile(connection){
-  // fs.readFile('MOCK.csv', 'utf8', function(err, data){
-  //     if(err){
-  //       console.log('FILE_READ_ERROR');
-  //       return;
-  //     }
-  //
-  //     connection.send(data,{binary:true});
-  //     console.log('CSV_TRANSFER_COMPLETE');
-  // });
 
-  var stream = fs.createReadStream('MOCK.csv');
-  stream.on('data', (chunk)=>{
-    connection.send(chunk, {binary: true});
+/****************
+/* Read data.json
+****************/
+
+var count = 0,
+buf = '';
+var ptr = 0;
+var t;
+var chunkArr = [];
+var totalCount = 0;
+function pushData(connection){
+
+  console.log("\n\nINFO: BEGIN_STREAM_JSON");
+
+  var stream = fs.createReadStream(__dirname+'/public/data/data.json',{ highWaterMark: 2 * 1024, flags: 'r', encoding: 'utf-8' });
+
+  stream.on("data",(chunk)=>{
+    buf+= chunk.toString();
+
+    pump(connection);
+
   });
-  stream.on('end',()=>{
-    console.log('INFO: CSV_TRANSFER_COMPLETE_OK');
+
+  stream.on("end", function(){
+    // end read
+    console.log("INFO: READ_COMPLETE_JSON");
+    count = 0;
   });
+
+
+}
+
+
+function pump(con){
+  var pos;
+  while( (pos=buf.indexOf('\n')) >= 0){
+    if (pos == 0) { // if there's more than one newline in a row, the buffer will now start with a newline
+            buf = buf.slice(1); // discard it
+            continue; // so that the next iteration will start with data
+        }
+    processLine(buf.slice(0,pos), con);
+    buf = buf.slice(pos+1);
+  }
+}
+
+function processLine(line, con) { // here's where we do something with a line
+
+    if (line[line.length-1] == '\r') line=line.substr(0,line.length-1); // discard CR (0x0D)
+
+    if (line.length > 0) { // ignore empty lines
+        var obj = JSON.parse(line); // parse the JSON
+        //console.log(obj); // do something with the data here!
+        //return line;
+
+        chunkArr.push(line);
+        totalCount = chunkArr.length;
+
+        if(!t){
+          t = setInterval(()=>{
+            if(ptr == totalCount){
+              clearInterval(t);
+              return;
+            }
+            console.log("READING_CHUNK: "+(++count));
+
+            if(con.readyState == 1){
+
+                con.send(chunkArr[ptr]);
+            }
+
+            ptr+=1;
+
+
+          }, 300);
+        }
+    }
+    //return '';
 }
 
 function newConnection(connection){
   //handle connection
   console.log('INFO: WS_CONNECTION_OK');
   connection.on('message', incomingMessageHandler);
-  sendCSVFile(connection);
+  pushData(connection);
 }
 
 
 // definitions end
-
-
-
-// on connection open
-ws.on('open', function(){
-  console.log('INFO: WS_INIT_OK');
-});
 
 
 // on connection
